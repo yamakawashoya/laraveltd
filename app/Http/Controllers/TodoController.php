f08e92350853eaceeb2eed4d6139a7a0770e8e29
yamakawashoya <119674662+yamakawashoya@users.noreply.github.com>
2023-02-27 17:55:32 +0900

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $todos = Todo::all();
+        return view('index',['todos' => $todos]);
+    }
+
+}
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-widthz, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>COACHTECH</title>
+</head>
+
+<style>
+
+</style>
+
+<body>
+<div class='todo'>
+  <div class='box'>
+    <p class='todo_list'>Todo List</p>
+    <div class='todo'>
+      <form action=""></form>
+      <table>
+        <tr>
+          <th>作成日</th>
+          <th>タスク名</th>
+          <th>更新</th>
+          <th>削除</th>
+        </tr>
+    </div>
+  </div>
+</div>
+</body>
+</html>
